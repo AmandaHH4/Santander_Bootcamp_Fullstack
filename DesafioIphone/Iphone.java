86d85824ae77d82b8388fd42ededccaeb2aeaf08
Amanda <amandahhhimler4@gmail.com>
2023-10-04 13:48:41 -0300

    fullstackDio

---
+++ DesafioIphone/Iphone.java
@@ -0,0 +1,90 @@
+// Interface para o Reprodutor Musical
+interface ReprodutorMusical {
+    void tocar();
+    void pausar();
+    void selecionarMusica(String musica);
+}
+
+// Interface para o Aparelho Telefônico
+interface AparelhoTelefonico {
+    void ligar(String numero);
+    void atender();
+    void iniciarConversaVoz();
+}
+
+// Interface para o Navegador na Internet
+interface NavegadorInternet {
+    void exibirPagina(String url);
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
+
+// Classe que representa o iPhone implementando as interfaces
+class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música...");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando a música: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarConversaVoz() {
+        System.out.println("Iniciando conversa de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Exemplo de uso do iPhone
+        iPhone iphone = new iPhone();
+        
+        // Reprodutor Musical
+        iphone.tocar();
+        iphone.pausar();
+        iphone.selecionarMusica("Música 1");
+        
+        // Aparelho Telefônico
+        iphone.ligar("123456789");
+        iphone.atender();
+        iphone.iniciarConversaVoz();
+        
+        // Navegador na Internet
+        iphone.exibirPagina("https://www.apple.com");
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+    }
+}
